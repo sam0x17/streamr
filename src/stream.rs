e919ad0e49fd9600efca10f018468811ae131ade
Sam Johnson <sam@durosoft.com>
2024-05-30 18:10:05 -0400

    WIP

--- src/stream.rs
+++ src/stream.rs
@@ -1,1 +1,19 @@
+use core::{fmt::Debug, hash::Hash, marker::PhantomData};
 
+pub trait Streamable: Copy + Clone + Debug + Hash + PartialEq + Eq {}
+
+impl<T> Streamable for T where T: Copy + Clone + Debug + Hash + PartialEq + Eq {}
+
+pub trait Node<T: Streamable = u8>: Default {
+    fn state(&self) -> &Stream<Self, T>;
+    fn state_mut(&mut self) -> &mut Stream<Self, T>;
+}
+
+pub struct 
+
+pub struct Stream<N: Node<T>, T: Streamable = u8> {
+    node: N,
+    limit: Option<u64>,
+    remaining: Option<u64>,
+    _phantom: PhantomData<T>,
+}
